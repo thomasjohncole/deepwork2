84a3c8c660e06dcde4aac0819874ac48f44a98ec
Tom Cole <colecode.ph@gmail.com>
2017-08-23 00:33:52 +0800

    Add custom converter and function to delete day
    Add custom converter class DateConverter to work.py. Add function deleteDay, which uses the route /delete/<date:work_date>/. Add template delete_day.html, which currently does nothing

--- work.py
+++ work.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine, func, update
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Dailyhours
 from datetime import date, datetime
+# add import for making custom converter class
+from werkzeug.routing import BaseConverter, ValidationError
 
 engine = create_engine('sqlite:///deepwork.db')
 Base.metadata.bind = engine
@@ -16,6 +18,25 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 # create an instance of the DBSession  object - to make a changes
 # to the database, we can call a method within the session
+
+# Add custom converter - https://stackoverflow.com/questions/31669864/date-in-flask-url
+class DateConverter(BaseConverter):
+    """Extracts a ISO8601 date from the path and validates it."""
+
+    regex = r'\d{4}-\d{2}-\d{2}'
+
+    def to_python(self, value):
+        try:
+            return datetime.strptime(value, '%Y-%m-%d').date()
+        except ValueError:
+            raise ValidationError()
+
+    def to_url(self, value):
+        return value.strftime('%Y-%m-%d')
+
+
+app.url_map.converters['date'] = DateConverter
+
 
 @app.route('/')
 def indexPage():
@@ -41,17 +62,19 @@ def addDay():
     else:
         return render_template('add_day.html')
 
-# @app.route('/delete/', methods=['GET', 'POST'])
-#def deleteDay(work_date):
-#    """ Page to delete a day row entry from the database """
-#    day_to_delete = session.query(Dailyhours).filter_by(work_date = work_date).one()
-#    if request.method == 'POST':
-#        session.delete(day_to_delete)
-#        # flash('%s Successfully Deleted' % restaurantToDelete.name)
-#        session.commit()
-#        return redirect(url_for('indexPage', restaurant_id = restaurant_id))
-#    else:
-#        return render_template('deleteRestaurant.html',restaurant = restaurantToDelete)
+@app.route('/delete/<date:work_date>/', methods=['GET', 'POST'])
+def deleteDay(work_date):
+    """ Page to delete a day row entry from the database """
+    day_to_delete = session.query(Dailyhours).filter_by(work_date = work_date).one()
+
+    if request.method == 'POST':
+        session.delete(day_to_delete)
+        # flash('%s Successfully Deleted' % restaurantToDelete.name)
+        session.commit()
+        return redirect(url_for('indexPage'))
+    else:
+         return render_template('delete_day.html', day_to_delete = day_to_delete)
+        # return render_template('add_day.html')
 
 
 
