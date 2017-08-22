ab898fbfe910678918c9282ae92ee6782fa46517
Tom Cole <colecode.ph@gmail.com>
2017-08-22 19:06:13 +0800

    Remove day field from database table
    The day field is not needed, because a weekday entry can be generated from the day work_date column, which is a date object, and so contains day of week information

--- database_setup.py
+++ database_setup.py
@@ -12,7 +12,6 @@ class Dailyhours(Base):
     __tablename__ = 'dailyhours'
 
     work_date = Column(Date, primary_key = True)
-    day = Column(String(10))
     hours_worked = Column(Float)
     remarks = Column(String(250))
 
