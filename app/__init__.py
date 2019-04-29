9bd96991b3b85790e3e6219a80a6cd0e7e55deb1
colecode.ph <colecode.ph@gmail.com>
2019-04-29 18:14:50 +0800

    Add config.py file for separation of concerns, using class Config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 # this is the __init__.py file
 from flask import Flask
+from config import Config
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+# lower case 'config' is the module name
+# upper case 'Config' is the class name
+app.config.from_object(Config)
+
 Bootstrap(app)
 
 from app import routes
